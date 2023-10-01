36fe0505f495ff5ecb10318a6208b1fe72a5c04f
Ajay Mishra <asap2656@gmail.com>
2023-10-01 09:55:47 +0530

    first java code

---
+++ lec1.java
@@ -0,0 +1,9 @@
+class Test{
+
+  // step-1 : compile and convert your code(.java file) to byte code(ie .class file) --> javac file-Name (this will create a .class byte code file)
+  // step-2 : run your code --> java class-Name
+  public static void main(String a[]){
+    System.out.print("hello Programmer !!");
+  }
+
+} 
