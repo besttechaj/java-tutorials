4f81074f5df7c3569d95589023134cbc23aa156d
Ajay Mishra <asap2656@gmail.com>
2024-01-01 01:37:35 +0530

    multi-dimensional array of same size

--- lec_8/lec8_Arrays.java
+++ lec_8/lec8_Arrays.java
@@ -22,11 +22,39 @@ public static void main(String[] args) {
   int num []= {21,22,34,45,67};
 
   //to get the first element inside array
-System.out.println(num[0]);
+// System.out.println(num[0]);
 
   // to print the array's elements
 for (int i = 0; i < num.length; i++) {
-  System.out.println(num[i]);
+  // System.out.println(num[i]); 
+}
 
-  // array of arrays: nested arrays
-}}}
+  // multidimensional array
+  int num1 [][]= new int[3][4];// we have 3 arrays inside that each will have 4 elements: 3*4=12elements
+
+// Providing the values to multi-dimensional array
+for (int i = 0; i < num1.length; i++) {
+    for (int j = 0; j < 4; j++) {
+      num1[i][j]=(int)(Math.random()*10);//math.random give decimal values less than 1
+      // System.out.println(num1[i][j]);
+    }
+  }
+
+  // printing the multi-dimensional array
+  for (int i = 0; i < num1.length; i++) {
+    for (int j = 0; j < 4; j++) {
+      // System.out.print(num1[i][j]+" ");
+    }
+    System.out.println();
+  }
+
+  //NEW WAY TO GET MULTI-DIMENSIONAL ARRAY
+  for (int m[]  : num1) {
+    System.out.println();//returns the arrays
+    for(int n: m){
+      System.out.print(n + "    ");//returns the arrays element
+    }
+  }
+
+}}
+
